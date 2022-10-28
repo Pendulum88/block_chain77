6ce3c3bf0fd1cef5d463ddee46f269c607e7a215
Pendulum88 <zladudrjs@naver.com>
2022-10-28 17:44:21 +0900

    10.28.

---
+++ 10.28.class/models/board.js
@@ -0,0 +1,33 @@
+const Sequelize = require("sequelize");
+
+module.exports = class Board extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init(
+      {
+        title: {
+          type: Sequelize.STRING(20),
+          allowNull: false,
+        },
+        text: {
+          type: Sequelize.TEXT,
+          allowNull: false,
+        },
+      },
+      {
+        sequelize,
+        timestamps: true,
+        underscored: true,
+        modelName: "Board",
+        tableName: "board",
+        paranoid: true,
+        charset: "utf8mb4",
+        collate: "utf8mb4_general_ci",
+      }
+    );
+  }
+
+  static associate(db) {
+    db.Board.belongsTo(db.User, { foreignKey: "user_id", targetKey: "id" });
+    db.Board.hasMany(db.Comment, { foreignKey: "board_id", sourceKey: "id" });
+  }
+};
